17acbe4d74d7a4f71fc99ce39d3cc9c814d05482
orChen89 <orooshch0@gmail.com>
2021-12-21 12:11:36 +0200

    Initial commit

---
+++ src/com/or/CountDownThread.java
@@ -0,0 +1,25 @@
+package com.or;
+
+public class CountDownThread extends Thread{
+
+    private final int rate;
+    private int counter;
+
+    public CountDownThread(int rate, int counter) {
+        this.rate = rate;
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println(counter);
+            counter--;
+            try {
+                sleep(rate);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/com/or/Main.java
@@ -0,0 +1,13 @@
+package com.or;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Thread count1 = new CountDownThread(5000, 10);
+        Thread count2 = new CountDownThread(2000, 30);
+
+        count1.start();
+        count2.start();
+    }
+}
